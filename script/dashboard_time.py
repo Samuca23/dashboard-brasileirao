a4be5fb787a46e554acf2d25374bb06377399b97
samuca23 <samuelchiodini@hotmail.com>
2023-08-23 18:51:43 -0300

    tratando os resultados do status do time

--- script/dashboard_time.py
+++ script/dashboard_time.py
@@ -15,18 +15,23 @@ def createSelecboxTime():
 def createDashboardTime(sigla):
     index_of_sigla = df_pred.index[df_pred['Time'] == sigla].tolist()[0]
     nome_time = getNomeTimeFromSigla(df_pred.loc[index_of_sigla, 'Time'])
-    rebaixamento = df_pred.loc[index_of_sigla, 'cl_o']
-    sulAmericana = df_pred.loc[index_of_sigla, 'cl_1']
-    libertadores= df_pred.loc[index_of_sigla, 'cl_2']
-    limbo = df_pred.loc[index_of_sigla, 'cl_3']
-    titulo = df_pred.loc[index_of_sigla, 'cl_4']
+    rebaixamento = trataValorDashboardTime(df_pred.loc[index_of_sigla, 'cl_o'])
+    sulAmericana = trataValorDashboardTime(df_pred.loc[index_of_sigla, 'cl_1'])
+    libertadores = trataValorDashboardTime(df_pred.loc[index_of_sigla, 'cl_2'])
+    limbo = trataValorDashboardTime(df_pred.loc[index_of_sigla, 'cl_3'])
+    titulo = trataValorDashboardTime(df_pred.loc[index_of_sigla, 'cl_4'])
 
     st.markdown(f"Status do **{nome_time}** no campeonato")
-    st.markdown(f"**Título:** {titulo}")
-    st.markdown(f"**Libertadores:** {libertadores}")
-    st.markdown(f"**Sul-Americana:** {sulAmericana}")
-    st.markdown(f"**Limbo:** {limbo}")
-    st.markdown(f"**Rebaixamento:** {rebaixamento}")
+    st.markdown(f"**Título:** {titulo}%")
+    st.markdown(f"**Libertadores:** {libertadores}%")
+    st.markdown(f"**Sul-Americana:** {sulAmericana}%")
+    st.markdown(f"**Limbo:** {limbo}%")
+    st.markdown(f"**Rebaixamento:** {rebaixamento}%")
+
+def trataValorDashboardTime(valor):
+    retorno = valor * 100
+
+    return round(retorno)
 
 def mainDashboardTime():
     createSelecboxTime()
